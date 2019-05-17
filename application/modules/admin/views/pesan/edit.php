23970def69dc4ff422a121c4c31385f0e16e1fc4
iwansafr <iwansafr@gmail.com>
2019-05-18 02:11:55 +0700

    jabatatan on perangkat list

--- application/modules/admin/views/pesan/edit.php
+++ application/modules/admin/views/pesan/edit.php
@@ -6,14 +6,20 @@
   </div>
   <div class="col-md-9">
 		<?php
+		$where = (!is_admin() || !is_root() || !is_kecamatan()) ? " AND (username LIKE 'kec_%' OR user_role_id =2)":'';
 		$this->zea->init('edit');
 		$this->zea->setTable('pesan');
 		$this->zea->addInput('sender','static');
 		$this->zea->setValue('sender',@intval($user['id']));
 		$this->zea->addInput('recipient','dropdown');
 		$this->zea->setLabel('recipient', 'Kirim Ke');
-		$this->zea->tableOptions('recipient','user','id','username',' active = 1 AND user_role_id > 1 AND id != '.$user['id']);
-		$this->zea->setFirstOption('recipient',['0'=>'Semua']);
+		$this->zea->tableOptions('recipient','user','id','username',' active = 1 AND user_role_id > 1 AND id != '.$user['id'].' '.$where);
+		if(is_admin() || is_root() || is_kecamatan())
+		{
+			$this->zea->setFirstOption('recipient',['0'=>'Semua']);
+		}else{
+			// $this->zea->setFirstOption('recipient',['-'=>'Pilih Penerima']);
+		}
 		$this->zea->addInput('title','text');
 		$this->zea->setLabel('title','Judul');
 		$this->zea->addInput('file','file');
