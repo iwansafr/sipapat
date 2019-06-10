7a4357c43b2ee94da12a61842f11200f1d445aee
iwansafr <iwansafr@gmail.com>
2019-06-10 07:58:26 +0700

    upper case username

--- application/modules/admin/views/templates/AdminLTE/sidebar.php
+++ application/modules/admin/views/templates/AdminLTE/sidebar.php
@@ -4,7 +4,7 @@
       <img src="<?php echo image_module('user', $user['id'].'/'.$user['image']) ?>" class="img-circle" alt="User Image">
     </div>
     <div class="pull-left info">
-      <p><?php echo $user['username']?></p>
+      <p><?php echo strtoupper($user['username'])?></p>
       <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
     </div>
   </div>
