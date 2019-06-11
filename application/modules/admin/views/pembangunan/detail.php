de6c1854fc258d5edc08dec3fed945d57efff9ea
iwansafr <iwansafr@gmail.com>
2019-06-11 11:38:01 +0700

    image size in detal

--- application/modules/admin/views/pembangunan/detail.php
+++ application/modules/admin/views/pembangunan/detail.php
@@ -84,7 +84,7 @@ if(!empty($data))
 							</div>
 						<?php else: ?>
 							<div class="row">
-								<div class="col-md-5">
+								<div class="col-md-4">
 									<table class="table table-hover table-striped">
 										<!-- <thead>
 											<tr>
