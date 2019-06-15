dd00d9dbf8c75761be52809b13b22f7ead1de61a
iwansafr <iwansafr@gmail.com>
2019-06-15 08:42:09 +0700

    upper case kecamatan title

--- application/modules/admin/views/perangkat/desa.php
+++ application/modules/admin/views/perangkat/desa.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <form action="<?php echo base_url('admin/perangkat/'.$group) ?>" method="get">
 	<div class="form-group">
-		<label>sortir perdesa</label>
+		<label>sortir perdesa <?php echo !empty(@$_GET['kec']) ? 'di kecamatan '.strtoupper($_GET['kec']) : ''; ?></label>
 	</div>
 	<div class="form-group form-inline">
 		<select class="form-control select2" name="desa_id">
--- application/modules/admin/views/perangkat/kecamatan.php
+++ application/modules/admin/views/perangkat/kecamatan.php
@@ -7,7 +7,7 @@
 		<select class="form-control select2" name="kec">
 			<option>pilih kecamatan</option>
 			<?php foreach ($kec_option as $key => $value): ?>
-				<option value="<?php echo str_replace('kec_','',$value['nama']) ?>"><?php echo str_replace('kec_','',$value['nama']) ?></option>
+				<option value="<?php echo str_replace('kec_','',$value['nama']) ?>"><?php echo strtoupper(str_replace('kec_','',$value['nama'])) ?></option>
 			<?php endforeach ?>
 		</select>
 		<button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
