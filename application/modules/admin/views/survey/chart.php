6cbd0e53fd751dcad39c089a2374ee0efabf59a4
iwansafr <iwansafr@gmail.com>
2019-06-29 21:07:34 +0700

    money icon on survey chart

--- application/modules/admin/views/survey/chart.php
+++ application/modules/admin/views/survey/chart.php
@@ -24,7 +24,7 @@
 
   <div class="col-md-3 col-sm-6 col-xs-12">
     <div class="info-box">
-      <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+      <span class="info-box-icon bg-green"><i class="fa fa-money"></i></span>
 
       <div class="info-box-content">
         <span class="info-box-text">DAPAT HONOR</span>
