203dbd6a9283dba08ae44ece003eb06462feb490
iwansafr <iwansafr@gmail.com>
2019-07-30 20:57:51 +0700

    info

---
+++ application/modules/admin/controllers/Info.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Info extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('esg_model');
+		$this->load->model('admin_model');
+		$this->load->model('sipapat_model');
+		$this->load->model('pengguna_model');
+		$this->load->library('esg');
+		$this->load->library('ZEA/zea');
+		$this->esg_model->init();
+	}
+
+	public function index()
+	{
+		echo phpinfo();
+	}
+}
