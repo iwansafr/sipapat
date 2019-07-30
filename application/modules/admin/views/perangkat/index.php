b3fd6ca8c0f2305272615e685279c769d0d0f383
iwansafr <iwansafr@gmail.com>
2019-07-30 20:41:17 +0700

    edit config and ignore

--- application/modules/admin/views/perangkat/index.php
+++ application/modules/admin/views/perangkat/index.php
@@ -57,7 +57,7 @@ if(is_root() || is_admin())
 	);
 }else{
 	$form->setHeading('<a href="'.base_url('admin/perangkat/'.$module[$kelompok].'/edit').'"><button class="btn btn-sm btn-warning"><i class="fa fa-plus-circle"></i></button></a>'.'data '.$module[$kelompok].' '.
-		'<a href="'.base_url('admin/perangkat/pdf/'.$module[$kelompok]).'" class="btn btn-sm btn-default"><i class="fa fa-file-pdf-o"></i>/<i class="fa fa-print"></i></a>'.
+		'<a target="_blank" href="'.base_url('admin/perangkat/pdf/'.$module[$kelompok]).'" class="btn btn-sm btn-default"><i class="fa fa-file-pdf-o"></i>/<i class="fa fa-print"></i></a>'.
 		'<a target="_blank" href="'.base_url('admin/perangkat/excel/'.$module[$kelompok]).'" class="btn btn-sm btn-default"><i class="fa fa-file-excel-o"></i></a>');
 }
 $form->addInput('desa_id','dropdown');
