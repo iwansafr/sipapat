67338601a46631211f48a162914ca51413624927
iwansafr <iwansafr@gmail.com>
2019-08-07 10:01:11 +0700

    updated api perangkat limit and pagination"

--- application/modules/api/controllers/Perangkat.php
+++ application/modules/api/controllers/Perangkat.php
@@ -13,6 +13,10 @@ class Perangkat extends CI_Controller
 	public function index()
 	{
 		$desa_id = @intval($_GET['d_id']);
+		$page = @intval($_GET['page']);
+		$page = (@intval($page) > 0 ) ? $page-1 : $page;
+		$limit = 12;
+		$limit = $limit*$page.','.$limit;
 		$where = '';
 		if(!empty($desa_id))
 		{
@@ -21,7 +25,21 @@ class Perangkat extends CI_Controller
 			$desa_id = '';
 		}
 
-		$data = $this->db->query("SELECT * FROM perangkat_desa where foto != '' AND jabatan = 1 AND kelompok = 1 $where ORDER BY id DESC LIMIT 12", $desa_id)->result_array();
+		$data = $this->db->query("SELECT * FROM perangkat_desa where foto != '' AND jabatan = 1 AND kelompok = 1 $where ORDER BY id DESC LIMIT $limit ", $desa_id)->result_array();
+		pr($this->db->last_query());
+		pr($data);die();
+		$desa_ids = [];
+		foreach ($data as $key => $value) 
+		{
+			foreach($value AS $vkey => $vvalue)
+			{
+				if($vkey=='desa_id')
+				{
+					$desa_ids[] = $value['desa_id'];
+				}
+			}
+		}
+		$desa_ids = array_unique($desa_ids);
 		$kelamin = ['Perempuan','Laki-laki'];
 		$agama = 
 			[
@@ -47,14 +65,25 @@ class Perangkat extends CI_Controller
 				'9'=>strtoupper('tamat sd/sederajat'),
 				'10'=>strtoupper('tidak/belum sekolah')
 			];
+
+		if(!empty($desa_ids))
+		{
+
+		}
+		$this->db->where_in('id', $desa_ids);
+		$desa_tmp = $this->db->get('desa')->result_array();
+		$desa = [];
+		foreach ($desa_tmp as $key => $value) 
+		{
+			$desa[$value['id']] = $value;
+		}
 		foreach ($data as $key => $value) 
 		{
 			$jabatan = $this->sipapat_model->get_jabatan($value['kelompok'], $value['jabatan']);
-			$desa = $this->sipapat_model->get_desa($value['desa_id']);
 			$data[$key]['foto'] = image_module('perangkat_desa',$value['id'].'/'.$value['foto']);
 			$data[$key]['jabatan'] = $jabatan['jabatan'];
 			$data[$key]['kelompok'] = $jabatan['kelompok'];
-			$data[$key]['desa'] = $desa;
+			$data[$key]['desa'] = $desa[$value['desa_id']];
 			$data[$key]['kelamin'] = $kelamin[$value['kelamin']];
 			$data[$key]['agama'] = $agama[$value['agama']];
 			$data[$key]['status_perkawinan'] = $status_perkawinan[$value['status_perkawinan']];
