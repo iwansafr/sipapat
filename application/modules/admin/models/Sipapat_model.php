47ae9a6dfe94639999b636406f76bc50a46b1792
iwansafr <iwansafr@gmail.com>
2019-09-10 19:25:25 +0700

    remove static meta

--- application/modules/admin/models/Sipapat_model.php
+++ application/modules/admin/models/Sipapat_model.php
@@ -10,20 +10,20 @@ class Sipapat_model extends CI_Model
 
 	public function set_meta($data = array())
 	{
-		if(empty($data) || !is_array($data))
-		{
-			$data = array(
-						'title' => 'sipapat',
-						'keyword' => 'software development',
-						'description' => 'PT media nusa perkasa',
-						'developer' => 'esoftgreat',
-						'author' => 'esoftgreat',
-						'email' => 'iwan@esoftgreat.com , iwansafr@gmail.com',
-						'phone' => '6285640510460',
-						'icon' => base_url('images/icon.png'),
-					);
-		}
-		$this->esg->set_esg('meta', $data);
+		// if(empty($data) || !is_array($data))
+		// {
+		// 	$data = array(
+		// 				'title' => 'sipapat',
+		// 				'keyword' => 'software development',
+		// 				'description' => 'PT media nusa perkasa',
+		// 				'developer' => 'esoftgreat',
+		// 				'author' => 'esoftgreat',
+		// 				'email' => 'iwan@esoftgreat.com , iwansafr@gmail.com',
+		// 				'phone' => '6285640510460',
+		// 				'icon' => base_url('images/icon.png'),
+		// 			);
+		// }
+		// $this->esg->set_esg('meta', $data);
 	}
 
 	public function get_desa_id()
--- application/modules/admin/views/index.php
+++ application/modules/admin/views/index.php
@@ -1,17 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 $this->esg->check_login();
-// pr($this->esg->get_esg());die();
-$data = array(
-						'title' => 'sipapat',
-						'keyword' => 'software development',
-						'description' => 'PT media nusa perkasa',
-						'developer' => 'esoftgreat',
-						'author' => 'esoftgreat',
-						'email' => 'iwan@esoftgreat.com , iwansafr@gmail.com',
-						'phone' => '6285640510460',
-						'icon' => base_url('images/icon.png'),
-					);
-$this->esg_model->set_meta($data);
+
 $mod['name'] = $this->router->fetch_class();
 $mod['task'] = $this->router->fetch_method();
 if($mod['name'] == 'admin' && $mod['task'] == 'index')
