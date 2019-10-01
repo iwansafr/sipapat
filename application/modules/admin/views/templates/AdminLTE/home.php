5563bd5d23ca144570a36b380106fffedea0f99b
iwansafr <iwansafr@gmail.com>
2019-10-01 08:39:27 +0700

    margin bottom box

--- application/modules/admin/views/templates/AdminLTE/home.php
+++ application/modules/admin/views/templates/AdminLTE/home.php
@@ -17,7 +17,7 @@ $pengumuman = $this->esg->get_config('pengumuman');
 		foreach ($dashboard as $key => $value) 
 		{
 			?>
-			<div class="col-md-3" style="height: 150px;">
+			<div class="col-md-3" style="height: 150px; margin-bottom: 2%;">
 				<div class="small-box" style="background:  <?php echo $value['color'] ?>; color:white;">
 				  <div class="inner">
 				    <h3><?php echo $value['title'] ?></h3>
