84f4b5d4d8c871f502b9eb10bb67c0a045bd4d12
iwansafr <iwansafr@gmail.com>
2019-10-21 15:08:28 +0700

    singkron zea with sipapat upload

--- application/modules/admin/views/sipapatconfig/kabupaten.php
+++ application/modules/admin/views/sipapatconfig/kabupaten.php
@@ -3,43 +3,25 @@
 if(is_root() || is_admin())
 {
 	$base_url_config = $this->esg->get_config(base_url());
+	$editable = true;
 	if(!empty($base_url_config))
 	{
-		$this->load->model('indonesia_model');
-		$indonesia['province'] = $this->indonesia_model->get_province_by_id($base_url_config['province_id']);
-		$indonesia['regency'] = $this->indonesia_model->get_regency_by_id($base_url_config['regency_id']);
-		?>
-		<div class="panel card panel-default card-default">
-			<div class="panel-heading card-header">
-				Config Kabupaten
-			</div>
-			<div class="panel-body card-body">
-				<table class="table table-hover">
-					<tr>
-						<td>provinsi</td>
-						<td>: <?php echo @$indonesia['province']['name'] ?></td>
-					</tr>
-					<tr>
-						<td>provinsi</td>
-						<td>: <?php echo @$indonesia['regency']['name'] ?></td>
-					</tr>
-					<tr>
-						<td>images</td>
-						<td>: <?php echo image_module('config', 'kabupaten_image') ?></td>
-					</tr>
-				</table>
-				
-			</div>
-			<div class="panel-footer card-footer">
-				
-			</div>
-		</div>
-		<?php
+		$editable = false;
+		if(!empty(@$_GET['edit']))
+		{
+			$editable = true;
+		}
 	}else{
+		$editable = true;
+	}
+
+	if($editable)
+	{
 		$form = new zea();
 		$form->init('param');
 		$form->setTable('config');
 		$form->setParamName(base_url());
+		$form->setHeading('<a href="'.base_url('admin/sipapatconfig/kabupaten').'" class="btn btn-warning btn-sm pull-right"><i class="fa fa-arrow-left"></i> kembali</a>');
 
 		$form->addInput('province_id','dropdown');
 		$form->setLabel('province_id','Provinsi');
@@ -52,9 +34,40 @@ if(is_root() || is_admin())
 		$form->addInput('image', 'upload');
 		$form->setAccept('image', 'image/jpeg,image/png');
 
-		$form->setDirImage('kabupaten_image');
+		$form->setDirImage('kabupaten_image_'.str_replace('/','_',base_url()));
 
 		$form->setFormName(base_url());
 		$form->form();
+	}else{
+		$this->load->model('indonesia_model');
+		$indonesia['province'] = $this->indonesia_model->get_province_by_id($base_url_config['province_id']);
+		$indonesia['regency'] = $this->indonesia_model->get_regency_by_id($base_url_config['regency_id']);
+		?>
+		<div class="panel card panel-default card-default">
+			<div class="panel-heading card-header">
+				Config Kabupaten <a href="<?php echo base_url('admin/sipapatconfig/kabupaten?edit=true') ?>" class="btn btn-warning btn-sm pull-right"><i class="fa fa-pencil-alt"></i> Edit</a>
+			</div>
+			<div class="panel-body card-body">
+				<table class="table table-hover">
+					<tr>
+						<td>provinsi</td>
+						<td>: <?php echo @$indonesia['province']['name'] ?></td>
+					</tr>
+					<tr>
+						<td>kabupaten</td>
+						<td>: <?php echo @str_replace('KABUPATEN', '', $indonesia['regency']['name']); ?></td>
+					</tr>
+					<tr>
+						<td>images</td>
+						<td>: <img src="<?php echo image_module('config', 'kabupaten_image_'.str_replace('/', '_', base_url()).'/'.$base_url_config['image']) ?>" class="img img-responsive img-fluid" style="margin-left: 2%;"></td>
+					</tr>
+				</table>
+				
+			</div>
+			<div class="panel-footer card-footer">
+				
+			</div>
+		</div>
+		<?php
 	}
 }
