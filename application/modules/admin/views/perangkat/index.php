ef81e0baf4f83d55b663b36b5f2a2419c77ac25c
iwansafr <iwansafr@gmail.com>
2019-11-02 01:29:24 +0700

    clean excel

--- application/modules/admin/views/perangkat/index.php
+++ application/modules/admin/views/perangkat/index.php
@@ -58,7 +58,7 @@ if(is_root() || is_admin())
 }else{
 	$form->setHeading('<a href="'.base_url('admin/perangkat/'.$module[$kelompok].'/edit').'"><button class="btn btn-sm btn-warning"><i class="fa fa-plus-circle"></i></button></a>'.'data '.$module[$kelompok].' '.
 		'<a target="_blank" href="'.base_url('admin/perangkat/pdf/'.$module[$kelompok]).'" class="btn btn-sm btn-default"><i class="fa fa-file-pdf-o"></i>/<i class="fa fa-print"></i></a>'.
-		'<a target="_blank" href="'.base_url('admin/perangkat/excel/'.$module[$kelompok]).'" class="btn btn-sm btn-default"><i class="fa fa-file-excel-o"></i></a>');
+		'<a target="_blank" href="'.base_url('admin/perangkat/clean_excel/'.$module[$kelompok]).'" class="btn btn-sm btn-default"><i class="fa fa-file-excel-o"></i></a>');
 }
 $form->addInput('desa_id','dropdown');
 $form->tableOptions('desa_id','desa','id','nama');
