647a06b97e4e96118a6efd97cb287ee7d84760ed
iwansafr <iwansafr@gmail.com>
2019-11-08 01:46:15 +0700

    rebuild query on kecamatan user list

--- application/modules/admin/views/pengguna/kecamatan_list.php
+++ application/modules/admin/views/pengguna/kecamatan_list.php
@@ -10,23 +10,22 @@ if(is_root() || is_admin())
   if(!empty($sipapat_config))
   {
     if(!empty($where)){
-      // $where .= ' AND desa.regency_id = '.$sipapat_config['regency_id'];
+      $where .= ' AND user_desa.regency_id = '.$sipapat_config['regency_id'];
     }else{
-      // $where = ' desa.regency_id = '.$sipapat_config['regency_id'];
+      $where = ' user_desa.regency_id = '.$sipapat_config['regency_id'];
     }
-    // $form->join('desa',
-    // 'ON(desa.district_id=user_desa.district_id)',
-    // '
-    // user_desa.id,
-    // user_desa.username,
-    // user_desa.user_role_id,
-    // user_desa.nama,
-    // user_desa.email,
-    // user_desa.sandi,
-    // user_desa.active,
-    // desa.kecamatan');
+    $form->join('districts',
+    'ON(districts.id=user_desa.district_id)',
+    '
+    user_desa.id,
+    user_desa.username,
+    user_desa.user_role_id,
+    user_desa.nama,
+    user_desa.email,
+    user_desa.sandi,
+    user_desa.active,
+    districts.name');
   }
-  $where = " username LIKE '%kec_%'";
   $form->setWhere($where);
   $form->search();
   $form->addInput('id','link');
@@ -34,7 +33,6 @@ if(is_root() || is_admin())
   $form->setPlaintext('id','detail');
   $form->setLink('id',base_url('admin/pengguna/detail'),'id');
   $form->setClearGet(['id']);
-  // $form->addInput('nama','plaintext');
   $form->addInput('username','plaintext');
   $form->addInput('user_role_id','dropdown');
   $form->setAttribute('user_role_id','disabled');
@@ -48,7 +46,7 @@ if(is_root() || is_admin())
     $form->addInput('sandi','plaintext');
   }
   
-  // $form->addInput('kecamatan','plaintext');
+  $form->addInput('kecamatan','plaintext');
 
   $form->setUrl('admin/pengguna/clear_kec_list');
   $form->setFormName('pengguna_list_roll');
@@ -59,9 +57,9 @@ if(is_root() || is_admin())
     $form->setEdit(TRUE);
     $form->setDelete(TRUE);
   }
+  $form->form();
   if(is_root())
   {
     pr($form->getData()['query']);
   }
-  $form->form();
 }
