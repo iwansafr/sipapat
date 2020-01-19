9cc6f3f1c491430e5de2be11e9c899744b7f8228
iwansafr <iwansafr@gmail.com>
2020-01-19 23:06:42 +0700

    bugfix potensi api

--- application/modules/api/models/Desa_model.php
+++ application/modules/api/models/Desa_model.php
@@ -134,126 +134,36 @@ class Desa_model extends CI_Model
 
 	public function tanpa_potensi()
 	{
-		$kelompok_id = ['Potensi'];
-		$kelompok_min = [1];
-
 		$data = array();
 		$desa = $this->db->query('SELECT id,nama FROM desa')->result_array();
 
-		$desa_potensi = $this->db->query('SELECT desa_id,kategori FROM potensi_desa order by desa_id ASC')->result_array();
-		$desa_tmp = array();
-
+		$desa_potensi = $this->db->query('SELECT desa_id,count(desa_id) AS total FROM potensi_desa GROUP BY desa_id order by desa_id ASC')->result_array();
+		$desa_tmp = [];
 		foreach ($desa_potensi as $key => $value) 
 		{
-			$desa_tmp[$value['kategori']][] = $value['desa_id'];
+			$desa_tmp[$value['desa_id']] = $value;
 		}
 
-		$kelompok = [];
-		foreach($kelompok_id AS $kkey => $kvalue)
+		foreach ($desa as $key => $value) 
 		{
-			$total = 1;
-			foreach ($desa_tmp[$kkey] as $key => $value) 
+			$total = @intval($desa_tmp[$value['id']]['total']);
+			if($total>0)
 			{
-				if($key>0)
-				{
-					if($desa_tmp[$kkey][$key] == $desa_tmp[$kkey][$key-1])
-					{
-						$total++;
-						$kelompok[$kkey][$value] = $total;
-					}else{
-						$total = 1;
-					}
-				}
-			}
-		}
-		$data_desa = array();
-		foreach ($kelompok_id as $kkey => $kvalue)
-		{
-			foreach ($desa as $key => $value) 
-			{
-				$total = @intval($kelompok[$kkey][$value['id']]);
-				$desa_tmp[$kkey] = array_unique($desa_tmp[$kkey]);
-				if(!in_array($value['id'], $desa_tmp[$kkey]))
-				{
-					$data_desa['uncomplete'][$kkey][] = 
-					[
-						'id'=>$value['id'],
-						'nama'=>$value['nama'],
-						'total' => $total
-					];
-				}else{
-					if($total >= $kelompok_min[$kkey])
-					{
-						$data_desa['complete'][$kkey][] = 
-						[
-							'id'=>$value['id'],
-							'nama'=>$value['nama'],
-							'total' => $total
-						];
-					}else if($total == 0){
-						$data_desa['uncomplete'][$kkey][] = 
-						[
-							'id'=>$value['id'],
-							'nama'=>$value['nama'],
-							'total' => $total
-						];
-					}else{
-						$data_desa['kurang'][$kkey][] = 
-						[
-							'id'=>$value['id'],
-							'nama'=>$value['nama'],
-							'total' => $total
-						];
-					}
-				}
-			}
-		}
-		if(!empty($data_desa))
-		{
-			if(!empty($get))
-			{
-					if(!empty($data_desa['uncomplete'][$get]))
-					{
-						$data['uncomplete'][$get]['data']  = $data_desa['uncomplete'][$get];
-						$data['uncomplete'][$get]['title'] = $kelompok_id[$get];
-						$data['uncomplete'][$get]['total'] = count($data_desa['uncomplete'][$get]);
-					}
-					if(!empty($data_desa['kurang'][$get]))
-					{
-						$data['kurang'][$get]['data']  = $data_desa['kurang'][$get];
-						$data['kurang'][$get]['title'] = $kelompok_id[$get];
-						$data['kurang'][$get]['total'] = count($data_desa['kurang'][$get]);
-					}
-					if(!empty($data_desa['complete'][$get]))
-					{
-						$data['complete'][$get]['data']  = $data_desa['complete'][$get];
-						$data['complete'][$get]['title'] = $kelompok_id[$get];
-						$data['complete'][$get]['total'] = count($data_desa['complete'][$get]);
-					}
+				$data_desa['complete'][] = 
+				[
+					'id'=>$value['id'],
+					'nama'=>$value['nama'],
+					'total' => $total
+				];
 			}else{
-				foreach ($kelompok_id as $key => $value)
-				{
-					if(!empty($data_desa['uncomplete'][$key]))
-					{
-						$data['uncomplete'][$key]['data']  = $data_desa['uncomplete'][$key];
-						$data['uncomplete'][$key]['title'] = $value;
-						$data['uncomplete'][$key]['total'] = count($data_desa['uncomplete'][$key]);
-					}
-					if(!empty($data_desa['kurang'][$key]))
-					{
-						$data['kurang'][$key]['data']  = $data_desa['kurang'][$key];
-						$data['kurang'][$key]['title'] = $value;
-						$data['kurang'][$key]['total'] = count($data_desa['kurang'][$key]);
-					}
-					if(!empty($data_desa['complete'][$key]))
-					{
-						$data['complete'][$key]['data']  = $data_desa['complete'][$key];
-						$data['complete'][$key]['title'] = $value;
-						$data['complete'][$key]['total'] = count($data_desa['complete'][$key]);
-					}
-				}
+				$data_desa['uncomplete'][] = 
+				[
+					'id'=>$value['id'],
+					'nama'=>$value['nama'],
+					'total' => $total
+				];
 			}
 		}
-		return $data;
+		return $data_desa;
 	}
 }
