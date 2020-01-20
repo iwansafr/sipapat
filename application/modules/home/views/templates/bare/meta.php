feabe33d32176465932e693baecfcad20bc9e49e
iwansafr <iwansafr@gmail.com>
2020-01-21 06:02:57 +0700

    scale

--- application/modules/home/views/templates/bare/meta.php
+++ application/modules/home/views/templates/bare/meta.php
@@ -1,4 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title><?php echo @$site['title'] ?> - <?php echo @$site['description'] ?></title>
 <link href="<?php echo $link_template ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!-- <link rel="stylesheet" href="<?php echo base_url('templates/AdminLTE'); ?>/bower_components/select2/dist/css/select2.min.css"> -->
