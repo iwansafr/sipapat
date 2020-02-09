c980ff13b7fbf71eaf3fa140f0ffd4878edc39ea
iwansafr <iwansafr@gmail.com>
2020-02-10 04:52:34 +0700

    pengguna all only for root

--- application/modules/admin/views/pengguna/all.php
+++ application/modules/admin/views/pengguna/all.php
@@ -1,31 +1,32 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$form = new zea();
+if(is_root())
+{
+	$form = new zea();
+	$form->setNumbering(true);
 
+	$form->search();
+	$form->init('roll');
+	$form->setTable('user_desa');
 
-$form->setNumbering(true);
+	$form->join('user_role',
+	    'ON(user_role.id=user_desa.user_role_id)',
+	    '
+	    user_desa.id,
+	    user_desa.username,
+	    user_desa.user_role_id,
+	    user_role.level,
+	    user_desa.nama,
+	    user_desa.email,
+	    user_desa.sandi,
+	    user_desa.active');
 
-$form->search();
-$form->init('roll');
-$form->setTable('user_desa');
+	$form->addInput('id','plaintext');
+	$form->addInput('username','plaintext');
+	$form->addInput('sandi','plaintext');
+	$form->addInput('level','plaintext');
 
-$form->join('user_role',
-    'ON(user_role.id=user_desa.user_role_id)',
-    '
-    user_desa.id,
-    user_desa.username,
-    user_desa.user_role_id,
-    user_role.level,
-    user_desa.nama,
-    user_desa.email,
-    user_desa.sandi,
-    user_desa.active');
+	$form->setUrl('admin/pengguna/clear_all');
 
-$form->addInput('id','plaintext');
-$form->addInput('username','plaintext');
-$form->addInput('sandi','plaintext');
-$form->addInput('level','plaintext');
-
-$form->setUrl('admin/pengguna/clear_all');
-
-$form->form();
+	$form->form();
+}
