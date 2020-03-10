d720f1f930ff6dca62f60dfc7504544ff0b510de
iwansafr <iwansafr@gmail.com>
2020-03-11 00:04:15 +0700

    absensi

--- application/modules/admin/views/perangkat/index.php
+++ application/modules/admin/views/perangkat/index.php
@@ -68,7 +68,8 @@ if(is_root() || is_admin())
 }else{
 	$form->setHeading('<a href="'.base_url('admin/perangkat/'.$module[$kelompok].'/edit').'"><button class="btn btn-sm btn-warning"><i class="fa fa-plus-circle"></i></button></a>'.'data '.$module[$kelompok].' '.
 		'<a target="_blank" href="'.base_url('admin/perangkat/pdf/'.$module[$kelompok]).$desa_id_get.'" class="btn btn-sm btn-default"><i class="fa fa-file-pdf-o"></i>/<i class="fa fa-print"></i></a>'.
-		'<a target="_blank" href="'.base_url('admin/perangkat/clean_excel/'.$module[$kelompok]).$desa_id_get.'" class="btn btn-sm btn-default"><i class="fa fa-file-excel-o"></i></a>');
+		'<a target="_blank" href="'.base_url('admin/perangkat/clean_excel/'.$module[$kelompok]).$desa_id_get.'" class="btn btn-sm btn-default"><i class="fa fa-file-excel-o"></i></a>'.
+		'<a target="_blank" href="'.base_url('admin/absensi/list/').'" class="btn btn-sm btn-default"><i class="fa fa-list"></i> Absensi</a>');
 }
 $form->addInput('desa_id','dropdown');
 $form->tableOptions('desa_id','desa','id','nama');
