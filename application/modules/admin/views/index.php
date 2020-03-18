a159e32d63acfd537b393193e98cb636b1dc9534
iwansafr <iwansafr@gmail.com>
2020-03-19 06:12:45 +0700

    remove server pr

--- application/modules/admin/views/index.php
+++ application/modules/admin/views/index.php
@@ -60,7 +60,6 @@ $allowed = TRUE;
 // }
 if($allowed)
 {
-	pr($_SERVER);
 	// pr($this->esg->get_esg());die();
 	$this->load->view('templates'.DIRECTORY_SEPARATOR.$this->esg->get_esg('templates')['admin_template'].DIRECTORY_SEPARATOR.'index', $this->esg->get_esg());
 }else{
