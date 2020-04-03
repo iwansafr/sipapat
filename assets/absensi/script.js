d6d360de3d66bda0fb165c566f3991dc43099ada
iwansafr <iwansafr@gmail.com>
2020-04-03 12:27:25 +0700

    location

--- assets/absensi/script.js
+++ assets/absensi/script.js
@@ -73,6 +73,21 @@ function terlambat(){
 	// btn_upload.classList.remove("d-none");
 }
 $(document).ready(function(){
+	function getLocation() {
+	  if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(showPosition);
+	  } else { 
+	    alert("perangkat anda tidak mendukung untuk menangkap lokasi anda");
+	  }
+	}
+
+	function showPosition(position) {
+		$("form").find(".panel-body").append("<label>LOKASI</label><br>Latitude: " + position.coords.latitude + 
+	  "<br>Longitude: " + position.coords.longitude+"<input type='hidden' name='koordinat' value='long:"+position.coords.longitude+",lat:"+position.coords.latitude+"'>");
+	}
+	getLocation();
+
+
 	function remove_alert(){
 		$('.alert').remove();
 		clearInterval(rm_msg);
