4abc39e98b4195f42fb5b18f695f4c0ff2a19ec2
iwansafr <iwansafr@gmail.com>
2020-04-04 09:11:14 +0700

    signin with google

--- application/modules/admin/views/user/login.php
+++ application/modules/admin/views/user/login.php
@@ -106,13 +106,16 @@
       </div>
     </form>
 
-    <!-- <div class="social-auth-links text-center">
+    <div class="social-auth-links text-center">
       <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+      <!-- <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
         Facebook</a>
       <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google+</a>
-    </div> -->
+        Google+</a> -->
+        <div class="g-signin2" data-onsuccess="onSignIn">
+          
+        </div>
+    </div>
     <!-- /.social-auth-links -->
 
     <!-- <a href="#">I forgot my password</a><br> -->
