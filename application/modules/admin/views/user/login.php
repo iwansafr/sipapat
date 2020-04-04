5650bc5a2c0972d637002359b8cfb15808cf52c8
iwansafr <iwansafr@gmail.com>
2020-04-04 09:20:59 +0700

    comment

--- application/modules/admin/views/user/login.php
+++ application/modules/admin/views/user/login.php
@@ -111,14 +111,14 @@
     </form>
 
     <div class="social-auth-links text-center">
-      <p>- OR -</p>
+      <!-- <p>- OR -</p> -->
       <!-- <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
         Facebook</a>
       <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
         Google+</a> -->
-        <div class="g-signin2" data-onsuccess="onSignIn">
+        <!-- <div class="g-signin2" data-onsuccess="onSignIn">
           
-        </div>
+        </div> -->
     </div>
     <!-- /.social-auth-links -->
 
