3d0e795de6ea08d60c01ffac978e561f485330c1
iwansafr <iwansafr@gmail.com>
2020-04-09 10:05:39 +0700

    corona get data

--- application/modules/admin/models/Corona_model.php
+++ application/modules/admin/models/Corona_model.php
@@ -26,7 +26,7 @@ class Corona_model extends CI_Model
 	public function get_data($desa_id = 0,$kec = '')
 	{
 		$where = '';
-		$data = $this->db->query('SELECT corona.nama,corona.umur,case jk WHEN 1 then "laki-laki" WHEN 2 then "Perempuan" END AS kelamin,corona.rt,corona.rw,desa.nama AS desa,corona.dari,corona.tgl AS tgl_kedatangan,corona.hp,corona.demam,corona.bpst AS `Batuk Pilek Sakit Tenggorokan`,corona.pkdpc AS `pernah kontak dg penderita covid` FROM corona INNER JOIN desa ON(desa.id=corona.desa_id) '.$where.' ORDER BY corona.desa_id ASC');
+		$data = [];
 		if(!empty($desa_id))
 		{
 			$where = ' WHERE desa_id = '.$desa_id;
@@ -36,6 +36,8 @@ class Corona_model extends CI_Model
 			{
 				$where = " WHERE kecamatan = ?";
 				$data = $this->db->query('SELECT corona.nama,corona.umur,case jk WHEN 1 then "laki-laki" WHEN 2 then "Perempuan" END AS kelamin,corona.rt,corona.rw,desa.nama AS desa,corona.dari,corona.tgl AS tgl_kedatangan,corona.hp,corona.demam,corona.bpst AS `Batuk Pilek Sakit Tenggorokan`,corona.pkdpc AS `pernah kontak dg penderita covid` FROM corona INNER JOIN desa ON(desa.id=corona.desa_id) '.$where.' ORDER BY corona.desa_id ASC',$kec);
+			}else{
+				$data = $this->db->query('SELECT corona.nama,corona.umur,case jk WHEN 1 then "laki-laki" WHEN 2 then "Perempuan" END AS kelamin,corona.rt,corona.rw,desa.nama AS desa,corona.dari,corona.tgl AS tgl_kedatangan,corona.hp,corona.demam,corona.bpst AS `Batuk Pilek Sakit Tenggorokan`,corona.pkdpc AS `pernah kontak dg penderita covid` FROM corona INNER JOIN desa ON(desa.id=corona.desa_id) '.$where.' ORDER BY corona.desa_id ASC');
 			}
 		}
 		return $data;
