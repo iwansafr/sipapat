4a9dcd948204650dc72514695ca3333fcfaa98db
iwansafr <iwansafr@gmail.com>
2020-04-15 11:15:46 +0700

    devel module

---
+++ application/modules/admin/controllers/Development.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Development extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->db->cache_off();
+		$this->load->model('esg_model');
+		$this->load->model('admin_model');
+		$this->load->model('sipapat_model');
+		$this->load->library('esg');
+		$this->load->library('ZEA/zea');
+		$this->esg_model->init();
+	}
+
+	public function index()
+	{
+		$this->load->view('index');
+	}
+}
---
+++ application/modules/admin/views/development/index.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+if(is_root())
+{
+	phpinfo();
+}
