4336838ec13bd1c7608e69d8a53fd7d84e77863e
iwansafr <iwansafr@gmail.com>
2020-05-27 14:59:16 +0700

    fpdf autoprint on dilan surat

--- application/modules/admin/controllers/Dilan.php
+++ application/modules/admin/controllers/Dilan.php
@@ -649,15 +649,16 @@ class Dilan extends CI_Controller{
 				}else{
 					$pdf->Cell(60,5,'',0,1,'C');
 				}
-				$pdf->Output('Surat_Keterangan_Pengantar.pdf','I');
 				if(!empty($_GET['print']))
 				{
+					$pdf->AutoPrint();
 					?>
 					<script type="text/javascript">
 						window.print();
 					</script>
 					<?php
 				}
+				$pdf->Output('Surat_Keterangan_Pengantar.pdf','I');
 			}
 		}
 	}
