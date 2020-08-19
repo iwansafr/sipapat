f8d5001ee82bf4842ecbe3a8476f18b085997319
iwansafr <iwansafr@gmail.com>
2020-08-19 11:54:11 +0700

    list apbdes

--- application/modules/admin/views/apbdes/list.php
+++ application/modules/admin/views/apbdes/list.php
@@ -1,27 +1,24 @@
 <?php
 
-if(is_desa())
-{
-	$form = new zea();
+$form = new zea();
 
-	$form->init('roll');
-	$form->setTable('apbdes');
-	$form->setNumbering();
-	$form->search();
-	$form->join('desa','on(desa.id=apbdes.desa_id)','apbdes.id,apbdes.infografi,apbdes.nominal,desa.nama,apbdes.tahun');
-	$form->addInput('id','hidden');
-	$form->addInput('tahun','plaintext');
-	$form->setType('tahun','number');
-	
-	$form->addInput('nama','plaintext');
+$form->init('roll');
+$form->setTable('apbdes');
+$form->setNumbering();
+$form->search();
+$form->join('desa','on(desa.id=apbdes.desa_id)','apbdes.id,apbdes.infografi,apbdes.nominal,desa.nama,apbdes.tahun');
+$form->addInput('id','hidden');
+$form->addInput('tahun','plaintext');
+$form->setType('tahun','number');
 
-	$form->addInput('infografi','thumbnail');
+$form->addInput('nama','plaintext');
 
-	$form->addInput('nominal','plaintext');
-	$form->settype('nominal','number');
-	$form->setEdit(true);
-	$form->setDelete(true);
-	$form->setUrl('admin/apbdes/clear_list');
+$form->addInput('infografi','thumbnail');
 
-	$form->form();
-}
+$form->addInput('nominal','plaintext');
+$form->settype('nominal','number');
+$form->setEdit(true);
+$form->setDelete(true);
+$form->setUrl('admin/apbdes/clear_list');
+
+$form->form();
