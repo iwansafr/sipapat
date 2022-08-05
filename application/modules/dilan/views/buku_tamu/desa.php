206a068b0c67608ec1015a598915f6ab5f7d80ac
esoftgreat <iwansafr@gmail.com>
2022-08-06 00:24:36 +0700

    mark footer

--- application/modules/dilan/views/buku_tamu/desa.php
+++ application/modules/dilan/views/buku_tamu/desa.php
@@ -10,23 +10,23 @@
   <meta content="buku-tamu" name="keywords">
 
   <!-- Favicons -->
-  <link href="/templates/Arsha/" rel="icon">
-  <link href="/templates/Arsha/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?php echo base_url();?>/templates/Arsha/" rel="icon">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="/templates/Arsha/assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="/templates/Arsha/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/templates/Arsha/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="/templates/Arsha/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="/templates/Arsha/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="/templates/Arsha/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="/templates/Arsha/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="/templates/Arsha/assets/css/style.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>/templates/Arsha/assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Arsha - v4.8.0
@@ -44,7 +44,7 @@
 
       <h1 class="logo me-auto"><a href="#">Buku Tamu</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="#" class="logo me-auto"><img src="/templates/Arsha/assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      <!-- <a href="#" class="logo me-auto"><img src="<?php echo base_url();?>/templates/Arsha/assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
@@ -74,7 +74,7 @@
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-          <img src="/templates/Arsha/assets/img/hero-img.png" class="img-fluid animated" alt="">
+          <img src="<?php echo base_url();?>/templates/Arsha/assets/img/hero-img.png" class="img-fluid animated" alt="">
         </div>
       </div>
     </div>
@@ -198,7 +198,7 @@
       </div>
     </div>
 
-    <div class="footer-top">
+    <!-- <div class="footer-top">
       <div class="container">
         <div class="row">
 
@@ -247,7 +247,7 @@
 
         </div>
       </div>
-    </div>
+    </div> -->
 
     <div class="container footer-bottom clearfix">
       <div class="copyright">
@@ -267,16 +267,16 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="/templates/Arsha/assets/vendor/aos/aos.js"></script>
-  <script src="/templates/Arsha/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="/templates/Arsha/assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="/templates/Arsha/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="/templates/Arsha/assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="/templates/Arsha/assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="/templates/Arsha/assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?php echo base_url();?>/templates/Arsha/assets/vendor/aos/aos.js"></script>
+  <script src="<?php echo base_url();?>/templates/Arsha/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url();?>/templates/Arsha/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?php echo base_url();?>/templates/Arsha/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php echo base_url();?>/templates/Arsha/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php echo base_url();?>/templates/Arsha/assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="<?php echo base_url();?>/templates/Arsha/assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="/templates/Arsha/assets/js/main.js"></script>
+  <script src="<?php echo base_url();?>/templates/Arsha/assets/js/main.js"></script>
 
 </body>
 
