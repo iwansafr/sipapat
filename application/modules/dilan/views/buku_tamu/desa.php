091463afd69cd7facde82809b2a9226906244173
iwansafr <iwansafr@gmail.com>
2022-08-05 09:36:43 +0700

    usefull link

--- application/modules/dilan/views/buku_tamu/desa.php
+++ application/modules/dilan/views/buku_tamu/desa.php
@@ -192,37 +192,35 @@
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
-            <!-- <h4>Useful Links</h4>
+            <h4>Menu</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul> -->
+              <li><i class="bx bx-chevron-right"></i> <a href="#contact">Buku Tamu</a></li>
+              
+            </ul>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
-            <!-- <h4>Our Services</h4>
+            <h4>Layanan Kami</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul> -->
+            </ul>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
-            <!-- <h4>Our Social Networks</h4>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+            <h4>Sosial Media Kami</h4>
+            <p>saling terhubung dengan cinta</p>
             <div class="social-links mt-3">
               <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
               <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
               <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
               <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
               <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div> -->
+            </div>
           </div>
 
         </div>
