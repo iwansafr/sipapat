72db0aa197b5df53c9e60724d9f419fc76585193
iwansafr <iwansafr@gmail.com>
2022-10-17 20:45:50 +0700

    sarpras and pekerjaan done

--- application/modules/admin/views/statistik/pekerjaan.php
+++ application/modules/admin/views/statistik/pekerjaan.php
@@ -1,2 +1,75 @@
 <?php
 $this->load->view('admin/statistik/menu');
+
+$pengguna = $this->pengguna_model->get_pengguna();
+
+if(is_desa())
+{
+    $desa = $this->sipapat_model->get_desa($pengguna['desa_id']);
+
+    $statistik_data = $this->statistik_model->getIdStatistikDesa($desa['id']);
+
+    $form = new Zea();
+    $form->init('edit');
+
+    if(!empty($statistik_data) && is_array($statistik_data)){
+        $form->setId($statistik_data[0]['id']);
+    }
+
+    $form->setHeading('Data Statistik Pendidikan Desa <b>'.$desa['nama'].'</b>');
+    $form->setEditStatus(false);
+    $form->setTable('statistik_penduduk');
+
+    $form->addInput('desa_id','static');
+    $form->setValue('desa_id', $pengguna['desa_id']);
+
+    $form->addInput('asn','text');
+    $form->setType('asn','number');
+    $form->setLabel('asn','Jumlah ASN');
+
+    $form->addInput('tni','text');
+    $form->setType('tni','number');
+    $form->setLabel('tni','Jumlah TNI');
+
+    $form->addInput('polri','text');
+    $form->setType('polri','number');
+    $form->setLabel('polri','Jumlah POLRI');
+
+    $form->addInput('karyawan_swasta','text');
+    $form->setType('karyawan_swasta','number');
+    $form->setLabel('karyawan_swasta','Jumlah Karyawan Swasta');
+
+    $form->addInput('karyawan_bumn','text');
+    $form->setType('karyawan_bumn','number');
+    $form->setLabel('karyawan_bumn','Jumlah Karyawan BUMN/BUMN');
+
+    $form->addInput('petani','text');
+    $form->setType('petani','number');
+    $form->setLabel('petani','Jumlah Petani');
+
+    $form->addInput('buruh_tani','text');
+    $form->setType('buruh_tani','number');
+    $form->setLabel('buruh_tani','Jumlah Buruh Tani');
+
+    $form->addInput('nelayan','text');
+    $form->setType('nelayan','number');
+    $form->setLabel('nelayan','Jumlah Nelayan');
+
+    $form->addInput('wiraswasta','text');
+    $form->setType('wiraswasta','number');
+    $form->setLabel('wiraswasta','Jumlah Wiraswasta');
+
+    $form->addInput('ibu_rumah_tangga','text');
+    $form->setType('ibu_rumah_tangga','number');
+    $form->setLabel('ibu_rumah_tangga','Jumlah Ibu Rumah Tangga');
+
+    $form->addInput('belum_bekerja','text');
+    $form->setType('belum_bekerja','number');
+    $form->setLabel('belum_bekerja','Jumlah Belum Bekerja');
+
+    $form->addInput('pekerjaan_lainnya','text');
+    $form->setType('pekerjaan_lainnya','number');
+    $form->setLabel('pekerjaan_lainnya','Jumlah Pekerja Lainnya');
+
+    $form->form();
+}
--- application/modules/admin/views/statistik/sarpras.php
+++ application/modules/admin/views/statistik/sarpras.php
@@ -33,7 +33,7 @@ if(is_desa())
 
     $form->addInput('faspen_sltp','text');
     $form->setType('faspen_sltp','number');
-    $form->setLabel('faspen_sltp','Jumlah SLTP');
+    $form->setLabel('faspen_sltp','Jumlah SLTP/SMP/MTS');
 
     $form->addInput('faspen_slta','text');
     $form->setType('faspen_slta','number');
@@ -51,9 +51,108 @@ if(is_desa())
     $form->endCollapse('faspen_perguruan_tinggi');
     // $form->setCollapse('faspen_tk',true);
 
-    
+    $form->addInput('faspem_kantor_desa','text');
+    $form->setType('faspem_kantor_desa','number');
+    $form->setLabel('faspem_kantor_desa','Jumlah Kantor Desa');
 
+    $form->addInput('faspem_balai_desa','text');
+    $form->setType('faspem_balai_desa','number');
+    $form->setLabel('faspem_balai_desa','Jumlah Balai Desa');
 
+    $form->addInput('faspem_perpus_desa','text');
+    $form->setType('faspem_perpus_desa','number');
+    $form->setLabel('faspem_perpus_desa','Jumlah Perpustakaan Desa');
+
+    $form->startCollapse('faspem_kantor_desa','Fasilitas Pemerintahan');
+    $form->endCollapse('faspem_perpus_desa');
+
+    $form->addInput('fasib_masjid','text');
+    $form->setType('fasib_masjid','number');
+    $form->setLabel('fasib_masjid','Jumlah Masjid');
+
+    $form->addInput('fasib_mushola','text');
+    $form->setType('fasib_mushola','number');
+    $form->setLabel('fasib_mushola','Jumlah Mushola');
+
+    $form->addInput('fasib_gereja','text');
+    $form->setType('fasib_gereja','number');
+    $form->setLabel('fasib_gereja','Jumlah Gereja');
+
+    $form->addInput('fasib_wihara','text');
+    $form->setType('fasib_wihara','number');
+    $form->setLabel('fasib_wihara','Jumlah Wihara');
+
+    $form->addInput('fasib_klenteng','text');
+    $form->setType('fasib_klenteng','number');
+    $form->setLabel('fasib_klenteng','Jumlah Klenteng');
+
+    $form->startCollapse('fasib_masjid','Fasilitas Ibadah');
+    $form->endCollapse('fasib_klenteng');
+
+    $form->addInput('fasek_pasar_desa','text');
+    $form->setType('fasek_pasar_desa','number');
+    $form->setLabel('fasek_pasar_desa','Jumlah Pasar Desa');
+
+    $form->addInput('fasek_toko','text');
+    $form->setType('fasek_toko','number');
+    $form->setLabel('fasek_toko','Jumlah Toko');
+
+    $form->addInput('fasek_swalayan','text');
+    $form->setType('fasek_swalayan','number');
+    $form->setLabel('fasek_swalayan','Jumlah Swalayan');
+
+    $form->addInput('fasek_restoran','text');
+    $form->setType('fasek_restoran','number');
+    $form->setLabel('fasek_restoran','Jumlah Restoran');
+
+    $form->addInput('fasek_rumah_makan','text');
+    $form->setType('fasek_rumah_makan','number');
+    $form->setLabel('fasek_rumah_makan','Jumlah Rumah Makan');
+
+    $form->addInput('fasek_warung_makan','text');
+    $form->setType('fasek_warung_makan','number');
+    $form->setLabel('fasek_warung_makan','Jumlah Warung Makan');
+
+    $form->startCollapse('fasek_pasar_desa','Fasilitas Ekonomi');
+    $form->endCollapse('fasek_warung_makan');
+
+    $form->addInput('faskes_pkd','text');
+    $form->setType('faskes_pkd','number');
+    $form->setLabel('faskes_pkd','Jumlah Pos Kesehatan Desa');
+
+    $form->addInput('faskes_puskesmas','text');
+    $form->setType('faskes_puskesmas','number');
+    $form->setLabel('faskes_puskesmas','Jumlah Puskesmas');
+
+    $form->addInput('faskes_klinik','text');
+    $form->setType('faskes_klinik','number');
+    $form->setLabel('faskes_klinik','Jumlah Klinik');
+
+    $form->addInput('faskes_dokter_praktik','text');
+    $form->setType('faskes_dokter_praktik','number');
+    $form->setLabel('faskes_dokter_praktik','Jumlah Dokter Praktik');
+
+    $form->startCollapse('faskes_pkd','Fasilitas Kesehatan');
+    $form->endCollapse('faskes_dokter_praktik');
+
+    $form->addInput('fasling_tpa','text');
+    $form->setType('fasling_tpa','number');
+    $form->setLabel('fasling_tpa','Jumlah BANK SAMPAH/TPA');
+
+    $form->addInput('fasling_makam','text');
+    $form->setType('fasling_makam','number');
+    $form->setLabel('fasling_makam','Jumlah Makam');
+
+    $form->addInput('fasling_lap_or','text');
+    $form->setType('fasling_lap_or','number');
+    $form->setLabel('fasling_lap_or','Jumlah Lapangan Olah Raga');
+
+    $form->addInput('fasling_pamsimas','text');
+    $form->setType('fasling_pamsimas','number');
+    $form->setLabel('fasling_pamsimas','Jumlah Pamsimas');
+
+    $form->startCollapse('fasling_tpa','Fasilitas Lingkungan/Umum');
+    $form->endCollapse('fasling_pamsimas');
 
     $form->form();
 }
